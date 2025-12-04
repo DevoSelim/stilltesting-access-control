9b06172a4ae847b7cac61c12839bb41fb624cf0a
Selim JABNOUN <selim.jabnoun@devoteam.com>
2025-12-04 16:18:11 +0100

    Feature : Adding some basic services & SG

--- infra/main.tf
+++ infra/main.tf
@@ -213,6 +213,105 @@ resource "aws_route_table_association" "db_c" {
   subnet_id      = aws_subnet.db_c.id
   route_table_id = aws_route_table.db.id
 }
+#Security Group
+##Security group ALB
+resource "aws_security_group" "alb" {
+  name        = "stilltesting-alb-sg"
+  description = "Security for ALB"
+  vpc_id      = aws_vpc.main.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "stilltesting-alb-sg"
+  }
+}
+##Security group Instance
+resource "aws_security_group" "app" {
+  name        = "stilltesting-app-sg"
+  description = "Security group for application instances & Lambdas"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "stilltesting-app-sg"
+  }
+}
+##Security group Aurora
+resource "aws_security_group" "db" {
+  name        = "stilltesting-db-sg"
+  description = "Security group for Aurora PostgreSQL"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "stilltesting-db-sg"
+  }
+}
+#DynamoDB
+resource "aws_dynamodb_table" "access_events" {
+  name         = "stilltesting-access-events"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key  = "pk"
+  range_key = "sk"
+
+  attribute {
+    name = "pk"
+    type = "S"
+  }
+
+  attribute {
+    name = "sk"
+    type = "S"
+  }
+
+  tags = {
+    Name = "stilltesting-access-events"
+  }
+}
+#SQS
+resource "aws_sqs_queue" "iot_events" {
+  name = "stilltesting-iot-events-queue"
+
+  visibility_timeout_seconds = 30
+
+  tags = {
+    Name = "stilltesting-iot-events-queue"
+  }
+}
+
+
 
 
 
