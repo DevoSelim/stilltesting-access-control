253ba810157c2e952d885f2e933c015b653eddf1
Selim JABNOUN <selim.jabnoun@devoteam.com>
2026-01-26 15:07:32 +0100

    adding lambda db migrate

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -186,3 +186,53 @@ resource "aws_lambda_permission" "apigw_invoke_employee" {
 
   source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/employee"
 }
+########################################
+# DB-Migrate
+########################################
+
+data "archive_file" "db_migrate_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/../lambdas/db_migrate"
+  output_path = "${path.module}/../lambdas/db_migrate.zip"
+}
+
+resource "aws_iam_role_policy" "db_migrate_secret" {
+  name = "stilltesting-db-migrate-secret"
+  role = aws_iam_role.lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = ["secretsmanager:GetSecretValue"],
+      Resource = aws_secretsmanager_secret.aurora_credentials.arn
+    }]
+  })
+}
+
+resource "aws_lambda_function" "db_migrate" {
+  function_name = "stilltesting-db-migrate"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "app.handler"
+  runtime       = "python3.12"
+
+  filename         = data.archive_file.db_migrate_zip.output_path
+  source_code_hash = data.archive_file.db_migrate_zip.output_base64sha256
+
+  timeout = 60
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private[0].id, aws_subnet.private[1].id, aws_subnet.private[2].id]
+    security_group_ids = [aws_security_group.sg_app.id]
+  }
+
+  environment {
+    variables = {
+      DB_SECRET_ARN = aws_secretsmanager_secret.aurora_credentials.arn
+      DB_HOST       = aws_rds_cluster.aurora.endpoint
+      DB_NAME       = "stilltesting"
+    }
+  }
+}
+
+
